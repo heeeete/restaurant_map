660365e3861e3a6d32d22952c89c981c5fe7580e
heeeete <mkoui@naver.com>
2023-03-20 02:29:46 +0900

    commit

---
+++ js/2.연산자.js
@@ -0,0 +1,5 @@
+let hight = 182;
+let standard_weight = (hight - 100) * 0.9;
+
+console.log("당신의 키는", hight, "이며 당신의 적정체중은", standard_weight, "입니다.");
+console.log(`당신의 키는 ${hight}cm 이며 당신의 적정체중은 ${standard_weight}kg 입니다.`)
